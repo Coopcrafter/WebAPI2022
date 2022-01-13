0095020ad99597b5e86886ab10d8c534333e8288
Coopcrafter <coopcrafter418@gmail.com>
2022-01-13 11:43:35 -0500

    End of Work

--- week1/server3/app.js
+++ week1/server3/app.js
@@ -20,8 +20,8 @@ http.createServer(function(request, response){
             response.writeHead(200,{"Content-type":"text/html"})
             response.write(data)
         }
-    }
     response.end()
+    }
 
 }).listen(port)
 
